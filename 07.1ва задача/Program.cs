cb628fb402c30b37c85da7e23cdd49e2920720c2
Proto68 <timkaq67@gmail.com>
2023-02-01 20:49:46 +0200

    Revert "Delete 07.1ва задача directory"
    This reverts commit 6cef54c7247fd855ed5b02f9ee384bda8d8370e1.

---
+++ 07.1ва задача/Program.cs
@@ -0,0 +1,12 @@
+﻿using _07._1ва_задача.Controllers;
+
+namespace _07._1ва_задача
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
