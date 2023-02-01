400e842e8f724ab340532c87d36fb4cbe86e17ea
Proto68 <timkaq67@gmail.com>
2023-02-01 12:58:00 +0200

    Created new project.

---
+++ 07.1ва задача/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._1ва_задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
